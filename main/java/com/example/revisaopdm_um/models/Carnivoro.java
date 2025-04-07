9d46a16f61208bdd0dde7eda1cff5011b9977648
Diana <03di.netuno@gmail.com>
2025-04-06 21:19:04 -0300

    Removendo arquivos deletados do projeto

--- main/java/com/example/revisaopdm_um/models/Carnivoro.java
+++
@@ -1,8 +0,0 @@
-package com.example.revisaopdm_um.models;
-
-//cria-se a interface carnívoro
-public interface Carnivoro {
-    /*cria-se o método caçar e adiciona-se default porque
-    não possui nenhum modificador de acesso especificado*/
-    default void cacar(){}
-}
